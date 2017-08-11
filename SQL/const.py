3ec46141cad32d1a1521ae60162a77ef722bcc0f
Михаил <ssosnovy1997@gmail.com>
2017-08-11 18:05:01 +0300

    debag
    Теперь оно почти работает (это не точно)

--- SQL/const.py
+++ SQL/const.py
@@ -1,4 +1,4 @@
-adr_con = 'C:\Users\RIGBY\Desktop\shop.db'
+adr_con = "C:\shop.db"
 token = "440360265:AAGvSazpPH0Hp17cAuy5q0Ut2borrfjFBsk"
 
 
--- SQL/sql.py
+++ SQL/sql.py
@@ -23,54 +23,84 @@ formatter = logging.Formatter("%(asctime)s | %(levelname)-7s | %(message)s")
 fh.setFormatter(formatter)
 logger.addHandler(fh)
 
-#klava
 
+#auth
 @bot.message_handler(commands=["auth"])
-def geophone(message):
+def auth(message):
     global auth
+    auth = 1
     if auth != 0:
         keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
         button_phone = types.KeyboardButton(text="Отправить номер телефона", request_contact=True)
         keyboard.add(button_phone)
         bot.send_message(message.chat.id,
-                        "Отправе нам свой текущий номер телефона для аунтификации",
+                        "Отправте нам свой текущиё номер телефона для аунтификации",
                         reply_markup=keyboard)
-        if str(message.text) == "89995252655":
-            bot.send_message(message.chat.id,
-                        "Аунтификация прошла успешно",
-                        )
-            auth = 0
-        else:
-            bot.send_message(message.chat.id,
-                             "Обратитесь к администратору",
-                             )
+
+@bot.message_handler(content_types=['contact'])
+def restart(message):
+    nomer = message.contact.phone_number
+    IDmes = message.contact.user_id
+    #IDuser = telebot.user.id
+
+    #bot.send_message(message.chat.id,
+    #                IDuser)
+    if (str(nomer) == "79175312197"): ####and (IDmes) == IDuser####
+        bot.send_message(message.chat.id,
+                         "Аунтификация прошла успешно, можно перейти к /start",
+                         )
+        global auth
+        auth = 0
+    else:
+        bot.send_message(message.chat.id,
+                         "Обратитесь к администратору",
+                         )
+
+
+
+#klava
 
 @bot.message_handler(commands=["start"])
-def text (message):
+def text(message):
     keyboard = types.ReplyKeyboardMarkup(row_width=1, one_time_keyboard=True)
-    button1 = types.KeyboardButton(text='Изменение')
-    button2 = types.KeyboardButton(text='Добавление')
-    button3 = types.KeyboardButton(text='Удаление')
+    button1 = types.KeyboardButton(text='Изменить')
+    button2 = types.KeyboardButton(text='Добавить')
+    button3 = types.KeyboardButton(text='Удалить')
     keyboard.add(button1, button2, button3)
+    bot.send_message(message.chat.id,
+                     "Что нужно сделать с товаром?",
+                     reply_markup=keyboard)
 
 
-@bot.message_handler(func=lambda item: item.text == 'Изменение', content_types=['text'])
-def change(item):
+@bot.message_handler(func=lambda item: item.text == 'Изменить', content_types=['text'])
+def change(message):
     if auth == 0:
         pass
     else:
-        print("Пожалуйста, авторизуйтесь (/auth)")
+        bot.send_message(message.chat.id,"Пожалуйста, авторизуйтесь (/auth)")
 
-@bot.message_handler(func=lambda item: item.text == 'Добавление', content_types=['text'])
-def add(item):
+@bot.message_handler(func=lambda item: item.text == 'Добавить', content_types=['text'])
+def add(message):
     if auth == 0:
         pass
     else:
-        print("Пожалуйста, авторизуйтесь (/auth)")
+        bot.send_message(message.chat.id,"Пожалуйста, авторизуйтесь (/auth)")
 
-@bot.message_handler(func=lambda item: item.text == 'Удаление', content_types=['text'])
-def dell(item):
+@bot.message_handler(func=lambda item: item.text == 'Удалить', content_types=['text'])
+def dell(message):
     if auth == 0:
         pass
     else:
-        print("Пожалуйста, авторизуйтесь (/auth)")
+        bot.send_message(message.chat.id,"Пожалуйста, авторизуйтесь (/auth)")
+
+
+
+
+
+
+@bot.message_handler(content_types=['text'])
+def restart(message):
+    bot.send_message(chat_id=message.chat.id, text='Выберите пункт меню или введите корректный запрос')
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
